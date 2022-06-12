42e5c2c3f93af2b803abf19c4457fa64248f5966
sjiang83 <sjiang83@fordham.edu>
2022-06-11 21:54:51 -0400

    branch_2Second commit

--- src/gitTest/test.java
+++ src/gitTest/test.java
@@ -7,5 +7,6 @@ public class test {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("冲突");
     }
 }
