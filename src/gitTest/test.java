7d21534606c9ef826ddd65b3046d7c2b9fee2851
sjiang83 <103335419+sjiang83@users.noreply.github.com>
2022-06-12 00:22:30 -0400

    pull test

--- src/gitTest/test.java
+++ src/gitTest/test.java
@@ -10,5 +10,6 @@ public class test {
         System.out.println("冲突");
         System.out.println("chongtu");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
